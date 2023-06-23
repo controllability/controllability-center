04860a7d28dce9de3482fadf9cb6682384d383be
Hongyu Cai <h.tsai@hotmail.com>
2023-06-23 12:04:35 -0400

    temp

--- index.js
+++ index.js
@@ -5,28 +5,100 @@ import https from 'https'
 import Koa from 'koa'
 import Router from '@koa/router'
 
+import {ChatClient, DeferredPromise} from './utils.js'
+
 const CONTROLLABILITY_CHAT_SERVER_URI = process.env.CONTROLLABILITY_CHAT_SERVER_URI
 assert(CONTROLLABILITY_CHAT_SERVER_URI)
 
+const CONTROLLABILITY_CHAT_SERVER_ADMIN_ACCESS_TOKEN = process.env.CONTROLLABILITY_CHAT_SERVER_ADMIN_ACCESS_TOKEN
+assert(CONTROLLABILITY_CHAT_SERVER_ADMIN_ACCESS_TOKEN)
 
-const app = new Koa()
-const router = new Router()
+const CONTROLLABILITY_CHAT_SERVER_OPENAI_ACCESS_TOKEN = process.env.CONTROLLABILITY_CHAT_SERVER_OPENAI_ACCESS_TOKEN
+assert(CONTROLLABILITY_CHAT_SERVER_OPENAI_ACCESS_TOKEN)
 
-router.all('/v1/bridge', async (ctx, next) => {
-    console.log(ctx.url)
-    console.log(ctx.hostname)
-    const channel_id = ctx.headers['x-controllability-channel_id']
+async function main() {
+    const channel_id_to_return_value_promise = new Map()
 
-    ctx.body = 'Hello World'
-})
+    const app = new Koa()
+    const router = new Router()
 
-app.use(router.routes()).use(router.allowedMethods())
+    const adminChatClient = new ChatClient(
+        CONTROLLABILITY_CHAT_SERVER_URI,
+        "@alice:localhost",
+        CONTROLLABILITY_CHAT_SERVER_ADMIN_ACCESS_TOKEN)
+    await adminChatClient.start_listening()
+    adminChatClient.on('Room.timeline', function (event, room, toStartOfTimeline) {
+        if (event.getType() !== "m.room.message") {
+            return
+        }
 
-const options = {
-    key: fs.readFileSync('nginx-selfsigned.key', 'utf8'),
-    cert: fs.readFileSync('nginx-selfsigned.crt', 'utf8')
-};
+        if (event.event.sender === adminChatClient.userId) {
+            return
+        }
 
-const port = 3000
-const server = https.createServer(options, app.callback());
-server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+        console.log(event.event.sender)
+        console.log(adminChatClient.userId)
+
+        const channel_id = room.roomId
+        const contentBody = event.event.content.body
+
+        const deferredPromise = channel_id_to_return_value_promise.get(channel_id)
+        assert(deferredPromise)
+        deferredPromise.resolve(contentBody)
+    })
+
+    const openaiChatClient = new ChatClient(
+        CONTROLLABILITY_CHAT_SERVER_URI,
+        "@openai:localhost",
+        CONTROLLABILITY_CHAT_SERVER_OPENAI_ACCESS_TOKEN)
+    await openaiChatClient.start_listening()
+    openaiChatClient.on('Room.timeline', async function (event, room, toStartOfTimeline) {
+        if (event.getType() !== "m.room.message") {
+            return
+        }
+
+        if (event.event.sender === openaiChatClient.userId) {
+            return
+        }
+
+        const channel_id = room.roomId
+        const content_body = event.event.content.body
+
+        // todo: make request to openai
+
+        await openaiChatClient.send(channel_id, "openai resp")
+    })
+
+
+    router.all('/v1/bridge', async (ctx, next) => {
+        // console.log(ctx.url)
+        // console.log(ctx.hostname)
+        // const channel_id = ctx.headers['x-controllability-channel_id']
+        const channel_id = "!DXc3Xs6BNg3Ikb7B:localhost"
+
+        const deferredPromise = new DeferredPromise()
+        channel_id_to_return_value_promise.set(channel_id, deferredPromise)
+
+        const body = {
+            method: ctx.method,
+            url: "todo",
+            headers: {},
+            data: {},
+        }
+
+        await adminChatClient.send(channel_id, JSON.stringify(body))
+        ctx.body = await deferredPromise.promise
+    })
+
+    app.use(router.routes()).use(router.allowedMethods())
+
+    const port = 3000
+    const options = {
+        key: fs.readFileSync('nginx-selfsigned.key', 'utf8'),
+        cert: fs.readFileSync('nginx-selfsigned.crt', 'utf8')
+    };
+    const server = https.createServer(options, app.callback());
+    server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+}
+
+main()
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 import * as sdk from "matrix-js-sdk";
+import assert from "assert";
 
 class DeferredPromise {
     constructor() {
@@ -10,10 +11,34 @@ class DeferredPromise {
 }
 
 class ChatClient {
-    constructor(url, accessToken) {
+    constructor(url, userId, accessToken) {
         this.client = sdk.createClient({
             baseUrl: url,
-            accessToken: accessToken
-        });
+            userId,
+            accessToken,
+        })
+        this.is_listening = false
+        this.userId = userId
+    }
+
+    async start_listening() {
+        await this.client.startClient({initialSyncLimit: 0})
+        this.is_listening = true
+    }
+
+    on(event, callback) {
+        assert(this.is_listening)
+        this.client.on(event, callback)
+    }
+
+    async send(roomId, body) {
+        const content = {
+            "msgtype": "m.text",
+            body
+        }
+
+        return this.client.sendEvent(roomId, "m.room.message", content, "")
     }
 }
+
+export {ChatClient, DeferredPromise}
